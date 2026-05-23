00653d6f073bce6c31a0ee8e2a02f027535f9f75
Vikram <3vikram2005@gmail.com>
2026-05-23 23:06:41 +0530

    Fix prisma config for deployment

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,21 +1,3 @@
 import "dotenv/config"
 
-import { defineConfig } from "prisma/config"
-
-export default defineConfig({
-
- schema:"prisma/schema.prisma",
-
- migrations:{
-
- seed:"tsx prisma/seed.ts"
-
- },
-
- datasource:{
-
- url:process.env.DATABASE_URL
-
- }
-
-})
+export default {}
